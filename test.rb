b0473d10b1f188c5dbac5c53abb282c6c936c5ef
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-27 18:29:00 +0000

    set config in basic test script

--- test.rb
+++ test.rb
@@ -3,6 +3,10 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 puts $LOAD_PATH
 
-require 'inkthreadable/order'
+require 'inkthreadable'
+
+Inkthreadable::Setup.configuration do |config|
+  config.secret_key = ENV['SECRET_KEY']
+end
 
 Inkthreadable::Order::get_json('order.php', {})
