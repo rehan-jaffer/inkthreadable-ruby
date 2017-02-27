a824c297f118681f2efd6350864fda7d65dc02cd
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-27 15:54:59 +0000

    namespaced classes

--- lib/inkthreadable.rb
+++ lib/inkthreadable.rb
@@ -2,10 +2,33 @@ require "./lib/inkthreadable/version"
 require "./lib/inkthreadable/order"
 
 module Inkthreadable
-  def self.configure
+
+  class Setup
+
+    class << self
+      attr_accessor :configuration
+    end
+
+
+    def self.configuration
+      @configuration ||= Configuration.new
+    end
+
+    def self.configure
+      yield(configuration)
+    end
+
+    class Configuration
+
+      attr_accessor :app_id
+      attr_accessor :secret_key
+
+      def initialize
+        @app_id = "APP-00100205"
+        @secret_key = "dkbwvjfZXgRJvZdu7h272gVMmFQ3Mv67"
+      end
+    end
+
   end
-  class Configuration
-    attr_accessor :app_id
-    attr_accessor :secret_key
-  end
+
 end
