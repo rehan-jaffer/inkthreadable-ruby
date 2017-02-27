554c375a75d1fff9ec9207d5edab2e88049eb507
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-27 12:05:51 +0000

    began adding API request system, a lot of this is being coded as part of a spike and as a result is a little messy, refactoring will follow!

--- lib/inkthreadable/api.rb
+++ lib/inkthreadable/api.rb
@@ -1,4 +1,35 @@
+# Inkthreadable API Request module
+# Inkthreadable's API provides both JSON and XML access. For the sake of homogeneity, only one will be used here (JSON)
+
 module Inkthreadable
-  class API
+  module API
+
+  API_BASE_URL = "http://api.inkthreadable.com/"
+
+  class Error < StandardError
+
+  end
+
+  class Request
+    def initialize(app_id)
+      @app_id = app_id or raise Exception, "No App ID provided, requests will fail"
+    end
+
+    # get a resource
+    # params
+    # resource - a valid inkthreadable resource
+    # params - a set of params built by RequestBuilder
+    def get(resource, params)
+
+    end
+  end
+
+  class Response
+  end    
+
+  class RequestBuilder
+    def self.build_params(list)
+    end
+    end
   end
 end
--- lib/inkthreadable/order.rb
+++ lib/inkthreadable/order.rb
@@ -1,7 +1,43 @@
 require 'inkthreadable/order'
 require 'inkthreadable/api'
+require 'open-uri'
+require 'json'
 
 module Inkthreadable
   class Order
+
+    def self.count
+    
+    end
+
+    def self.all
+    end
+
+    def self.find
+    end
+
+
+    # this all needs to get moved into API module asap, coding this during a spike
+    def self.get_json(resource, params)
+
+        query_string = []
+        params.each_pair do |k,v|
+          query_string << "#{k}=#{v}"
+        end
+        query_string = query_string.join("&")
+
+        data = URI('https://www.inkthreadable.co.uk/api/orders.php').read
+        begin
+          parsed_data = JSON.parse(data)
+          if parsed_data.has_key?("error")
+            raise Inkthreadable::API::Error, parsed_data["error"]
+          end
+          parsed_data
+        rescue JSON::ParserError
+          fail "Could not parse request JSON #{res.body}"
+        end
+
+      end
+
   end
 end 
