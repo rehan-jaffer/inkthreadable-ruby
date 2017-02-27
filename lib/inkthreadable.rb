17ca1e4473247db3a97b385f57a14197d777dd40
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-27 12:06:18 +0000

    misc updates

--- lib/inkthreadable.rb
+++ lib/inkthreadable.rb
@@ -1,5 +1,11 @@
-require "inkthreadable/version"
-require "inkthreadable/order"
+require "./lib/inkthreadable/version"
+require "./lib/inkthreadable/order"
 
 module Inkthreadable
+  def self.configure
+  end
+  class Configuration
+    attr_accessor :app_id
+    attr_accessor :secret_key
+  end
 end
