b8e7f0f4c6110ba9508a4b1aeda2ff504bacd622
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-28 10:57:09 +0000

    added rudimentary methods for using API, removed get_json which has been moved to the get() function in the API library

--- lib/inkthreadable/order.rb
+++ lib/inkthreadable/order.rb
@@ -8,41 +8,22 @@ module Inkthreadable
   class Order
 
     def self.count
-    
+      response = Inkthreadable::API::Request.get("orders/count.php", {})
+      return response["orders"].length
     end
 
-    def self.all
+    def self.all(page=1)
+      puts Inkthreadable::API::Request.get("orders.php", {page: page}) 
     end
 
-    def self.find
+    def self.find(id)
+      puts Inkthreadable::API::Request.get("order.php", {id: id})
     end
 
-
-    # this all needs to get moved into API module asap, coding this during a spike
-    def self.get_json(resource, params)
-
-        params ||= []
-        query_string = []
-        params["AppId"] = Inkthreadable::Setup.configuration.app_id
-        params.each_pair do |k,v|
-          query_string << "#{k}=#{v}"
-        end
-        query_string = query_string.join("&")
-        signature = Digest::SHA1.hexdigest(query_string + Inkthreadable::Setup.configuration.secret_key)
-        query_string += "&Signature=#{signature}"
-        
-        data = URI("https://www.inkthreadable.co.uk/api/orders.php?#{query_string}").read
-        begin
-          parsed_data = JSON.parse(data)
-          if parsed_data.has_key?("error")
-            raise Inkthreadable::API::Error, parsed_data["error"]
-          end
-          parsed_data
-        rescue JSON::ParserError
-          fail "Could not parse request JSON #{res.body}"
-        end
-
-      end
+    # readable - created_at, type, status, deleted
+    # writeable - brand, billing_address, shipping_address, items, external_id
+    def self.create(props)
+    end
 
   end
 end 
