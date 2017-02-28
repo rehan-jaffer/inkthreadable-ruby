e760277633970daf2bfce576bebc26965c862e50
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-28 10:58:46 +0000

    move test to new namespace

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 module Inkthreadable
-  describe Configuration do
-    describe "#app_id" do
-      it "can set app_id" do
-        config = Configuration.new
-        config.app_id = "valid app_id string"
-        expect(config.app_id).to eq "valid app_id string"
+    describe Setup do
+      describe "#app_id" do
+        it "can set app_id" do
+          config = Inkthreadable::Setup::Configuration.new
+          config.app_id = "valid app_id string"
+          expect(config.app_id).to eq "valid app_id string"
+        end
       end
     end
-  end
 end
