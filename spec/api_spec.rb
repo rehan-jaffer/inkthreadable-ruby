14d247d993104b92f058eb3c652f6a8bea60aa6b
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-28 12:02:27 +0000

    test API request minor methods

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 describe Inkthreadable::API do
   describe Inkthreadable::API::Request do
+
+    describe "#transform_params" do
+      it "transforms a hash into query string" do
+        expect(Inkthreadable::API::Request.transform_params({sample: "variable"})).to eq "sample=variable"
+      end
+      it "encodes html entities properly" do
+        expect(Inkthreadable::API::Request.transform_params({sample: "variable two"})).to eq "sample=variable+two"
+      end
+    end
+
+    describe "#sign_query_string" do
+
+    end
+
   end
 
   describe Inkthreadable::API::Response do
