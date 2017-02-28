13b0308148d5bf0b3bf74a27d95555c74df49e04
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-28 10:58:29 +0000

    get function moved to API file

--- lib/inkthreadable/api.rb
+++ lib/inkthreadable/api.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 # Inkthreadable API Request module
 # Inkthreadable's API provides both JSON and XML access. For the sake of homogeneity, only one will be used here (JSON)
 
@@ -11,8 +13,31 @@ module Inkthreadable
   end
 
   class Request
-    def initialize(app_id)
-      @app_id = app_id or raise Exception, "No App ID provided, requests will fail"
+
+    def self.get(resource, params)
+
+        params ||= []
+        query_string = []
+        params["AppId"] = Inkthreadable::Setup.configuration.app_id
+        params.each_pair do |k,v|
+          query_string << "#{k}=#{v}"
+        end
+        query_string = query_string.join("&")
+        signature = Digest::SHA1.hexdigest(query_string + Inkthreadable::Setup.configuration.secret_key)
+        query_string += "&Signature=#{signature}"
+
+        data = URI("https://www.inkthreadable.co.uk/api/orders.php?#{query_string}").read
+        pp data
+        begin
+          parsed_data = JSON.parse(data)
+          if parsed_data.has_key?("error")
+            raise Inkthreadable::API::Error, parsed_data["error"]
+          end
+          parsed_data
+        rescue JSON::ParserError
+          fail "Could not parse request JSON #{res.body}"
+        end
+
     end
 
     # get a resource
