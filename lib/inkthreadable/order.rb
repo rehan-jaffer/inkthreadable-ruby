c074d59372ba04eec09bed0e59fc5be71a3eb96f
Rehan Jaffer <ray@thelondonvandal.com>
2017-02-28 11:32:02 +0000

    rubocop-related changes

--- lib/inkthreadable/order.rb
+++ lib/inkthreadable/order.rb
@@ -6,24 +6,22 @@ require 'digest/sha1'
 
 module Inkthreadable
   class Order
-
     def self.count
-      response = Inkthreadable::API::Request.get("orders/count.php", {})
-      return response["orders"].length
+      response = Inkthreadable::API::Request.get('orders/count.php', {})
+      response['orders'].length
     end
 
-    def self.all(page=1)
-      puts Inkthreadable::API::Request.get("orders.php", {page: page}) 
+    def self.all(page = 1)
+      puts Inkthreadable::API::Request.get('orders.php', page: page)
     end
 
     def self.find(id)
-      puts Inkthreadable::API::Request.get("order.php", {id: id})
+      puts Inkthreadable::API::Request.get('order.php', id: id)
     end
 
     # readable - created_at, type, status, deleted
     # writeable - brand, billing_address, shipping_address, items, external_id
-    def self.create(props)
+    def self.create(_props)
     end
-
   end
-end 
+end
