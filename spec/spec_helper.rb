05befddf995612f6c79530c24637f2aa2dd45d8c
Rehan Jaffer <ray@thelondonvandal.com>
2017-03-02 12:37:32 +0000

    added VCR config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 require 'simplecov'
+require 'fakeweb'
+require 'vcr'
 SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'inkthreadable'
 
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :fakeweb # or :fakeweb
+end
